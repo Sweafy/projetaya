63f0e49eb46b79dd51ae8ebce8f2352fc90028e2
Sweafy <159787976+Sweafy@users.noreply.github.com>
2024-04-23 00:23:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToPage(pageUrl) {
+    // Redirige vers la page spécifiée
+    window.location.href = pageUrl;
+}
+
+function goToHomePage() {
+    // Redirige vers la page d'accueil (index.html)
+    window.location.href = 'index.html';
+}
